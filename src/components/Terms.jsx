a8ee047aef87e6934db2cadf56d4da1bea5eaf83
DadMalice <brentvineza@gmail.com>
2023-11-30 10:27:39 +0800

    Added under construction terms page

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -8,9 +8,17 @@ const Terms = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <div className='mt-20 mb-5 mx-10 pt-20 pb-5 px-10'>
-                <h1>Terms</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates aperiam delectus, fuga ad quidem ab sit accusantium qui tempora odio, facere doloremque. Molestias animi, praesentium officiis sequi nobis natus eius pariatur velit cum, et, sed reiciendis dicta ipsum doloremque illum optio reprehenderit temporibus! Aspernatur odio dolorum blanditiis cumque quisquam id illo, voluptates, quibusdam, necessitatibus quasi aperiam temporibus earum tempore veniam harum placeat explicabo corrupti amet ducimus ratione. Sequi, doloremque quasi minima possimus modi dolore similique! Commodi, quod totam. Possimus non doloremque consectetur mollitia, voluptas qui eaque perferendis maxime culpa ad neque aut, expedita ex! Quam tempora earum fugiat maxime nam.</p>
+            <div className="animated-gradient-background">
+                <div className="h-full flex flex-col justify-center items-center">
+                    <div className="py-10 px-8 text-center lg:w-1/2 w-9/12 bg-black bg-opacity-25 rounded-xl">
+                        <h1 className="mt-10 mb-10 lg:text-6xl xs:text-5xl text-2xl font-bold leading-tight text-white">
+                            Page Under Construction
+                        </h1>
+                        <p className="-mt-4 mb-5 text-base text-white md:text-lg text-center">
+                        Exciting updates are in progress! Our terms page is currently undergoing improvements to better serve you. We appreciate your patience. For urgent inquiries, please contact us at (562) 245-7580. Thank you for understanding!
+                        </p>
+                    </div>
+                </div>
             </div>
         </motion.div>
     )
